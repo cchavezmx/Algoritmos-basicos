63d7c83dc47da084c8887f64276ae50e8a0d882d
cchavezmx <cchavezmx@outlook.com>
2022-02-10 19:32:42 -0600

    primer ejercicio de arboles

--- tree/main.js
+++ tree/main.js
@@ -26,4 +26,49 @@ b.left = d
 b.right = e
 c.right = f
 
-console.log(a)
+// TODO Escribe una función, depthFirstValues, 
+// que tome la raíz de un árbol binario. 
+// La función debe devolver una array que contenga todos los valores del árbol 
+// en orden de profundidad. 
+
+// que son los falsy values
+// https://developer.mozilla.org/en-US/docs/Glossary/Falsy
+
+const depthFirstValues = (root) => {
+  const stack = [root]
+  const values = []
+  let counter = 0
+
+  while (stack.length > 0) {
+      // guardar en una variable el nodo que extraigo de stack
+      const current = stack.pop()
+      // actualizo mi counter para efectos de observacion
+      ++counter
+      console.log(current, counter)
+      // guardamos el valor del nodo en el array de values
+      values.push(current.val)
+
+      if(current.right) stack.push(current.right)
+      if(current.left) stack.push(current.left)
+  }
+
+  return values
+}
+
+console.log(depthFirstValues(a))
+
+// const nodo = {
+//   val: "A",
+//   right: "NODO B",
+//   left: null
+// }
+// RECUERDA: que el if verifica que la condicion sea true o false
+// true false
+// falsys
+// if(){
+// 	//  si la condicion es verdadera
+// } else {
+// //   si la condicion es falsa
+// }
+
+// console.log(Boolean(nodo.left))
