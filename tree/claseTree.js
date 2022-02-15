1a76b715ec41b27ca3f28871b42fb4ac9ab1aeca
cchavezmx <cchavezmx@outlook.com>
2022-02-14 19:28:14 -0600

    se añade ejercio de add de arbol

--- tree/claseTree.js
+++ tree/claseTree.js
@@ -9,12 +9,78 @@
 // los numeros mayores a la derecha
 
 // target es el valor que quiero buscar
+class Node{
+  constructor(value, left, right){
+    this.value = value
+    this.left = left
+    this.right = right
+  }
+}
+
+
+
 class Tree{
   constructor(){
     this.root = null
   }
-  add(valor)
-  contain(target)
+  add(newValue){
+    //TODO solo permitir numeros
+    if(typeof newValue !== 'number'){
+      alert('solo se permiten numeros')
+    }
+    //TODO los numeros no deben repetirse
+
+    // crear un nodo
+    let newNode = new Node(newValue, null, null)
+
+    // saber si ya existe un nodo en la raiz
+    if (this.root === null){
+      return this.root = newNode
+    }
+
+    // tratar de que cada linea de nuestro codigo haga una operacion a la vez
+    let current = this.root
+    let stop = true
+
+    while(stop){
+      if (current.value > newValue){
+        // como el valor es menor lo tenemos que acomodar a la izqueda
+        if (current.left !== null){
+          // si ya esta ocupado
+          current = current.left
+        } else {
+          current.left = newNode
+          stop = false
+        }
+
+      } else {
+        // inspeccionar a la derecha
+        if (current.value < newValue){
+          //
+          if (current.right !== null){
+            current = current.right
+          } else {
+            current.right = newNode
+            stop = false
+          }
+        }
+      }
+    }
+
+  }
+  // contain(target)
 }
 
-arbol.contains(80);
+const arbol = new Tree()
+console.log(arbol, '1')
+arbol.add(40)
+console.log(arbol, '2')
+arbol.add(60)
+console.log(arbol, '3')
+arbol.add(30)
+console.log(arbol, '4')
+arbol.add(20)
+console.log(arbol, '5')
+arbol.add(70)
+console.log(arbol, '6')
+
